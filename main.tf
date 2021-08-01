dda2fba8bfcc56ded79dfda0a5edd72590d31b5f
Sean Brown <rbitshifte@gmail.com>
2021-08-01 08:44:38 -0400

    Initial TF infra

---
+++ main.tf
@@ -0,0 +1,24 @@
+# Require TF version to be same as or greater than 0.12.13
+terraform {
+  required_version = ">=0.12.13"
+  backend "s3" {
+    bucket         = "tf-state-rbitshifte"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
+}
+
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.36.0"
+}
+
+# Call the seed_module to build our ADO seed info
+module "bootstrap" {
+  source                      = "./modules/bootstrap"
+  name_of_s3_bucket           = "tf-state-rbitshifte"
+  dynamo_db_table_name        = "aws-locks"
+}
